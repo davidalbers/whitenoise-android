d20a0a01c411ccaab6f6cee4a6ef7b2bcbcbf5e5
David Albers <davidgeorgea@gmail.com>
2022-04-23 07:20:56 -0500

    Read preference for wave interval

--- app/src/main/java/dalbers/com/noise/shared/UserPreferences.kt
+++ app/src/main/java/dalbers/com/noise/shared/UserPreferences.kt
@@ -9,10 +9,21 @@ const val PREF_PLAY_OVER = "pref_play_over"
 
 interface UserPreferences {
     fun playOver(): Boolean
+    fun waveIntervalMillis(): Int
 }
 
 class UserPreferencesImpl(
     private val sharedPreferences: SharedPreferences,
 ) : UserPreferences {
     override fun playOver(): Boolean = sharedPreferences.getBoolean(PREF_PLAY_OVER, false)
+    override fun waveIntervalMillis(): Int {
+        return when (sharedPreferences.getInt(PREF_OSCILLATE_INTERVAL_KEY, 0)) {
+            0 -> 8000
+            1 -> 10000
+            2 -> 12000
+            3 -> 15000
+            4 -> 30000
+            else -> error("Unexpected preference")
+        }
+    }
 }

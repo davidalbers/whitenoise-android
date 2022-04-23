8bc22b55580ccd10ca918110dfd10f5800837a7a
David Albers <davidgeorgea@gmail.com>
2022-04-23 07:23:23 -0500

    Read wave interval preference (#35)
    * Read preference for wave interval
    
    * Rename var

--- app/src/main/java/dalbers/com/noise/shared/UserPreferences.kt
+++ app/src/main/java/dalbers/com/noise/shared/UserPreferences.kt
@@ -4,15 +4,26 @@ import android.content.SharedPreferences
 
 
 const val PREF_USE_DARK_MODE_KEY = "pref_use_dark_mode"
-const val PREF_OSCILLATE_INTERVAL_KEY = "pref_oscillate_interval"
+const val PREF_WAVE_INTERVAL_KEY = "pref_oscillate_interval"
 const val PREF_PLAY_OVER = "pref_play_over"
 
 interface UserPreferences {
     fun playOver(): Boolean
+    fun waveIntervalMillis(): Int
 }
 
 class UserPreferencesImpl(
     private val sharedPreferences: SharedPreferences,
 ) : UserPreferences {
     override fun playOver(): Boolean = sharedPreferences.getBoolean(PREF_PLAY_OVER, false)
+    override fun waveIntervalMillis(): Int {
+        return when (sharedPreferences.getInt(PREF_WAVE_INTERVAL_KEY, 0)) {
+            0 -> 8000
+            1 -> 10000
+            2 -> 12000
+            3 -> 15000
+            4 -> 30000
+            else -> error("Unexpected preference")
+        }
+    }
 }
